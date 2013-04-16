4855fe91db4776ea70aea2e4e8d5b53323a9266c
Jackson Maslow <jackson.maslow@gmail.com>
2013-04-16 13:13:44 -0400

    Forgot to add API key

--- knowledge.js
+++ knowledge.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-  $("#final_span").suggest({ "key" : "<your api key>"});
+  $("#final_span").suggest({ "key" : "AIzaSyDBrXTAJYcC78EXRKgwVs3FbxR5W6NQ8CI"});
+  console.log("suggesting");
 });
